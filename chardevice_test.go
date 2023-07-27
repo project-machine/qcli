505a0cc0a9082411cf21171e4e6b09d512635c82
Ashwin Gopalan <agopalan@berkeley.edu>
2023-07-27 15:40:07 -0700

    ran gofmt again

--- chardevice_test.go
+++ chardevice_test.go
@@ -7,8 +7,8 @@ var (
 	deviceCharDeviceBackendSocket   = "-chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off"
 	deviceCharDeviceBackendStdioMux = "-chardev stdio,id=serial0,mux=on,signal=off"
 	deviceCharDeviceMultiple        = "-chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off -chardev socket,id=monitor0,path=/tmp/monitor.sock,server=on,wait=off"
-	deviceCharDevicePCIDriver		= "-serial none -chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off -device pci-serial,id=pciser0,chardev=serial0"
-	deviceCharDevicePCIDriver2x		= "-serial none -chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off -device pci-serial-2x,id=pciser0,chardev1=serial0"
+	deviceCharDevicePCIDriver       = "-serial none -chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off -device pci-serial,id=pciser0,chardev=serial0"
+	deviceCharDevicePCIDriver2x     = "-serial none -chardev socket,id=serial0,path=/tmp/console.sock,server=on,wait=off -device pci-serial-2x,id=pciser0,chardev1=serial0"
 )
 
 func TestBadCharDevice(t *testing.T) {
@@ -84,17 +84,17 @@ func TestAppendMultipleCharDevices(t *testing.T) {
 
 func TestAppendPCIDriver1x(t *testing.T) {
 	c := &Config{}
-	serial := CharDevice {
-		Driver: PCISerialDevice,
+	serial := CharDevice{
+		Driver:  PCISerialDevice,
 		Backend: Socket,
-		ID: "serial0",
-		Path: "/tmp/console.sock",
+		ID:      "serial0",
+		Path:    "/tmp/console.sock",
 	}
-	pcidev := SerialDevice {
-		Driver:        PCISerialDevice,
-		ID: 		   "pciser0",
-		ChardevIDs:	   []string{"serial0"},
-		MaxPorts: 		1,
+	pcidev := SerialDevice{
+		Driver:     PCISerialDevice,
+		ID:         "pciser0",
+		ChardevIDs: []string{"serial0"},
+		MaxPorts:   1,
 	}
 	c.CharDevices = []CharDevice{serial}
 	c.SerialDevices = []SerialDevice{pcidev}
@@ -103,17 +103,17 @@ func TestAppendPCIDriver1x(t *testing.T) {
 
 func TestAppendPCIDriver2x1(t *testing.T) {
 	c := &Config{}
-	serial := CharDevice {
-		Driver: PCISerialDevice,
+	serial := CharDevice{
+		Driver:  PCISerialDevice,
 		Backend: Socket,
-		ID: "serial0",
-		Path: "/tmp/console.sock",
+		ID:      "serial0",
+		Path:    "/tmp/console.sock",
 	}
-	pcidev := SerialDevice {
-		Driver:        PCISerialDevice,
-		ID: 		   "pciser0",
-		ChardevIDs:	   []string{"serial0"},
-		MaxPorts: 		2,
+	pcidev := SerialDevice{
+		Driver:     PCISerialDevice,
+		ID:         "pciser0",
+		ChardevIDs: []string{"serial0"},
+		MaxPorts:   2,
 	}
 	c.CharDevices = []CharDevice{serial}
 	c.SerialDevices = []SerialDevice{pcidev}
